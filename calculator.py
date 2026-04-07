0c9b22dac086e1a66b198058d6eca3d456e7c6d5
2025sl93062-lab <2025sl93062@wilp.bits-pilani.ac.in>
2026-04-07 17:32:05 +0530

    Introduced a bug in the code

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ def divide(a,b):
         return None
     return a/b
 
-if__name__=="__main__":
+if __name__ == "__main__":
     print("Running tests...")
     assert add(2, 3) ==5
     assert subtract(4, 3) ==1
